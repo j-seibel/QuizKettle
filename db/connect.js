ba6673e2f29c568d40bad0222443ebfe1eb8dd4c
j-seibel <107728907+j-seibel@users.noreply.github.com>
2022-07-14 20:32:31 -0500

    Setting up heroku variables

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 require("dotenv").config()
 
-console.log(DB_URI);
+const aws = require('aws-sdk');
+
+let s3 = new aws.S3({
+  DB_URI: process.env.DB_URI,
+});
+
+console.log(s3.DB_URI);
 
 
 
 // connects to database
 async function QuestionDB(){
     console.log("connected to db");
-    return mongoose.connect(process.env.DB_URI);
+    return mongoose.connect(s3.DB_URI);
  }
 
  
